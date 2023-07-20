a391fd542d03ef6c807019fc47f1e3403c38b3bb
Krishna Kumar <112097854+Krishna1518@users.noreply.github.com>
2023-07-20 18:57:28 +0530

    App1.java
    version1.1.2

--- App1.java
+++ App1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class App1
 {
 	static Scanner sc=new Scanner (System.in);
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception
 	{
 		System.out.println("\t\t\tWelcome To My Application");
 		System.out.println("1.Student ID's\n2.Laptop Details\n3.Exit");
@@ -10,9 +10,11 @@ class App1
 		switch (input_01)
 		{
 		case 1 :{
+			Thread.sleep(1000);
 			System.out.println("Select The ID's");
 			System.out.println("1.ID-36\n2.ID-268\n3.ID-456");
 			int input_02=sc.nextInt();
+			Thread.sleep(1000);
 			switch (input_02)
 			{
 			case 1 :{
@@ -24,11 +26,12 @@ class App1
 			case 3 :{
 				System.out.println("Student name: Dinga\nStudent ID  : ID-456\nYOP         : 2004");
 				break;}
-		    default:System.out.println("Input Mis-Match");
+		    default:System.out.println("\t\t\tInput Mis-Match");
 			
 			}
 			break;}
 		case 2 :{
+			Thread.sleep(1000);
 			System.out.println("Select Your Fav Lap"); //start with this
             System.out.println("1.Dell\n2.Acer\n3.Hp");
 			int input_03=sc.nextInt();
@@ -43,14 +46,17 @@ class App1
 			case 3 :{
 				System.out.println("Laptop name : Hp\nRam         : 4GB\nStorage     : 1024GB HDD");
 				break;}
-			default : System.out.println("Input Mis-Match");
+			default : System.out.println("\t\t\tInput Mis-Match");
 			
 			}
 			break;}
 		case 3 :{
+			Thread.sleep(2000);
 			System.out.println("\t\t\t Thank You For Your Visit");
 			break;}
-		default : System.out.println("Input Mis-match");
+		default : 
+		Thread.sleep(1000);	
+		System.out.println("\t\t\tInput Mis-match");
 		
 		}
 	}
